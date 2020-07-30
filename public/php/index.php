229446ecfb4af274105cda66527ada74e21fe6c7
fort3 <okonfortunesam@gmail.com>
2020-07-30 18:19:06 +0200

    diasble get in touch

--- public/php/index.php
+++ public/php/index.php
@@ -1,32 +1,92 @@
 <?php
-if(!empty($_POST['website'])) {
-        die();
-        echo '<script>console.log("DIED")</script>';
-	}
+// if(!empty($_POST['website'])) {
+//         die();
+//         echo '<script>console.log("DIED")</script>';
+// 	}
  
-	else if(isset($_POST['submit']) || isset($_POST['submit-2'])) {
+	// if(isset($_POST['submit']) || isset($_POST['submit-2'])) {
 
-        $from = 'Customer Request';
-        $to = 'okonfortunesam@gmail.com';
+    //     $from = 'Customer Request';
+    //     $to = 'okonfortunesam@gmail.com';
 
-            if(isset($_POST['submit-2'])){
-                $subject = 'Customer Request || Get In Touch!';
-            }else{
-                $subject = 'Customer Request';
-            }
+    //         if(isset($_POST['submit-2'])){
+    //             $subject = 'Customer Request || Get In Touch!';
+    //         }else{
+    //             $subject = 'Customer Request';
+    //         }
         
-        $cusName = $_POST['cus_name'];
-        $cusEmail = $_POST['cus_email'];
-        $cusPhone = $_POST['cus_phone'];
-        $cusMessage = $_POST['cus_statement'];
+    //     $cusName = $_POST['cus_name'];
+    //     $cusEmail = $_POST['cus_email'];
+    //     $cusPhone = $_POST['cus_phone'];
+    //     $cusMessage = $_POST['cus_statement'];
 
-        $body = "NAME: $cusName
-                EMAIL: $cusEmail
-                PHONE: $cusPhone
-                Message: $cusMessage";
+    //     $body = "NAME: $cusName
+    //             EMAIL: $cusEmail
+    //             PHONE: $cusPhone
+    //             Message: $cusMessage";
 
-        mail($to, $subject, $body, $from);
+    //     mail($to, $subject, $body, $from);
 
-        header("Status: 200");
-    }
+    //     header("Status: 200");
+    // }
+    if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['cus_name'];
+$visitor_email = $_POST['cus_email'];
+$visitor_phone = $_POST['cus_phone'];
+$message = $_POST['cus_statement'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'okonfortunesam@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+    
+$to = "okonfortunesam@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: ./index.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
 ?>
